c55d177d41386964b748e38e52a25f7bb2dac87b
haru <yesihyezi4@naver.com>
2024-03-12 18:30:10 +0900

    check

---
+++ 4p4p/js/common.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+  navigationTooltips: ['main', 'contents', 'footer'],
+  css3: true,
+  scrollingSpeed: 1000,
+  navigation: true,
+  scrollOverflow: false,
+  responsiveWidth: 884,
+  slidesNavigation: true,
+  responsiveHeight: 330,
+  dragAndMove: true,
+  dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+  controlArrows: false
+});
+
